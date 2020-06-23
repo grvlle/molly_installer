2059dee1e86e9ea321c1b791a91b6129f9345800
vito <gralle89@gmail.com>
2020-06-23 13:05:24 +0200

    Introduced regex to address the incompatibility issues with packaging data in wails data formats

--- backend/install/uninstall.go
+++ backend/install/uninstall.go
@@ -1,25 +1,32 @@
 package install
 
 import (
-	"fmt"
 	"path"
+	"regexp"
 	"runtime"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // Uninstall will Uninstall the application by removing the files located
 // in the .dag directory:
 //   'Molly Wallet.lnk'   cl-wallet.jar   mollywallet.exe   tmp
 //   cl-keytool.jar      install.log     store.db          wallet.log
+//   update.exe
 // And also removing the shortcuts on Windows.
 func (i *Install) Uninstall() {
 
-	files := make([]string, 12)
-	files = append(files, "update.log", "wallet.log", "store.db", "cl-keytool.jar.tmp", "cl-keytool.jar", "cl-wallet.jar", "cl-wallet.jar.tmp", "mollywallet.zip", "mollywallet.zip.tmp", "Molly Wallet.lnk", "mollywallet.exe")
+	updateBinary := "update" + i.OSSpecificSettings.fileExt
 
+	files := make([]string, 13)
+	files = append(files, "update.log", updateBinary, "wallet.log", "store.db", "cl-keytool.jar.tmp", "cl-keytool.jar", "cl-wallet.jar", "cl-wallet.jar.tmp", "mollywallet.zip", "mollywallet.zip.tmp", "Molly Wallet.lnk", "mollywallet.exe")
+
+	log.Infoln("Removing dependencies...")
 	err := removeFiles(i.dagFolderPath, files)
 	if err != nil {
 		i.sendErrorNotification("Error:", convertErrorToString(err))
-		fmt.Println(err)
+		log.Errorf("Error: %v", err)
 	}
 
 	folders := make([]string, 3)
@@ -28,17 +35,20 @@ func (i *Install) Uninstall() {
 	err = removeFolders(folders)
 	if err != nil {
 		i.sendErrorNotification("Error:", convertErrorToString(err))
-		fmt.Println(err)
+		log.Errorf("Error: %v", err)
 	}
 
+	log.Infoln("Removing shortcuts on Windows...")
 	if runtime.GOOS == "windows" {
 		err := removeFile(i.OSSpecificSettings.startMenuPath, "Molly Wallet.lnk")
 		if err != nil {
 			i.sendErrorNotification("Unable to remove shortcut from start menu", convertErrorToString(err))
+			log.Errorf("Error: %v", err)
 		}
 		err = removeFile(i.OSSpecificSettings.desktopPath, "Molly Wallet.lnk")
 		if err != nil {
 			i.sendErrorNotification("Unable to remove shortcut from desktop", convertErrorToString(err))
+			log.Errorf("Error: %v", err)
 		}
 	}
 
@@ -48,8 +58,22 @@ func (i *Install) Uninstall() {
 
 }
 
+// strip non-regex complient chars and return clean error string
 func convertErrorToString(err error) string {
-	bytes := []byte(err.Error())
-	errString := string(bytes[0:10])
+	if err == nil {
+		return ""
+	}
+
+	chars := []string{"]", "^", "\\\\", "[", ".", "(", ")", "-"}
+	r := strings.Join(chars, "")
+	errString := err.Error()
+	re := regexp.MustCompile("[" + r + "]+")
+	errString = re.ReplaceAllString(errString, "")
+
+	if len(errString) >= 15 {
+		bytes := []byte(errString)
+		return string(bytes[0:15])
+	}
+
 	return errString
 }
